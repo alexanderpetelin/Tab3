6042b0f201534a546ed114b9685aa2be0f7c9556
Alexander Petelin <aepetelin@gmail.com>
2017-08-20 18:15:12 +0700

    add todo

--- app/src/main/java/tsu/tab/MainActivity.java
+++ app/src/main/java/tsu/tab/MainActivity.java
@@ -40,6 +40,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // TODO: 20.08.2017 Hello world 
+                
     }
 
     @Override
